b95cae1c6e3d217d7e57dc869d2b70d3f6634878
Antonio de la Torre <antonio@codesai.com>
2020-12-03 18:44:32 +0100

    Examples

--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -10,3 +10,32 @@ public class RoverTest {
         assertThat(true, is(true));
     }
 }
+
+
+// Given face "N", when receives "r",  then "E"
+// Given face "S", when receives "r",
+// Given face "E"
+// Given face "W"
+// Given face "N", when receives "l",  then "W"
+// Given face "S", when receives "l",
+// Given face "E"
+// Given face "W"
+
+// Given (0,0,"N") , when receives "f", then (0,1,"N")
+// Given (0,0,"S") , when receives "f", then (0,-1,"S")
+// ...
+
+// Given (0,0,"N") , when receives "b", then (0,-1,"N")
+// ...
+
+// Given (1,1,"E") , when receives "b", then (0,-1,"N")
+
+
+// Given xxx  , when receives ""
+// Given xxx  , when receives "f"
+// Given xxx  , when receives "ff"
+// Given xxx  , when receives "x"
+
+
+
+
